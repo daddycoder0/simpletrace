d3fdb9f4d1d9f7d1982e9d930ce6f892258f63d5
daddycoder0 <daddycoder0@gmail.com>
2017-06-08 14:11:03 +0100

    - don't send anything to the output unless there will be a visual change

--- Progress.h
+++ Progress.h
@@ -8,31 +8,43 @@ class Progress
 		{
 			m_name = name;
 			m_progress = 0.f;
-			cout << name << ":" << endl;
+			m_intProgress = -1;
 		}
 
 		void UpdateProgress(float progress)
 		{
 			m_progress = progress;
-			cout << "\r[";
-			unsigned int bW = 55;
-			unsigned int actBarW = bW - 2;
-			for (unsigned int i=0; i< actBarW;i++)
-			{
-				cout << (((float)i / (float)actBarW < m_progress) ? "#" : " ");
-			}
-			
-			cout << "] " << int(m_progress * 100) << "%";
 
-			if (m_progress >= 1.f)
+			int p = int(m_progress * 100);
+
+			if (p != m_intProgress)
 			{
-				cout << endl << endl;
+				if (m_intProgress < 0)
+				{
+					cout << m_name << ":" << endl;
+				}
+				m_intProgress = p;
+				cout << "\r[";
+				unsigned int bW = 55;
+				unsigned int actBarW = bW - 2;
+				for (unsigned int i = 0; i< actBarW;i++)
+				{
+					cout << (((float)i / (float)actBarW < m_progress) ? "#" : " ");
+				}
+
+				cout << "] " << m_intProgress << "%";
+
+				if (m_progress >= 1.f)
+				{
+					cout << endl << endl;
+				}
+				cout << flush;
 			}
-			cout << flush;
 		}
 	private:
 
 		float m_progress;
+		int m_intProgress;
 		const char* m_name;
 
 };
